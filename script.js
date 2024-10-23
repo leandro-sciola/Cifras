79d99a21624900f0da3c50a4abc5ba62f903333e
Leandro Sciola <leandrosciola@gmail.com>
2024-10-23 16:26:42 -0300

    Updated

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const src = "https://github.com/leandro-sciola/cifras/raw/refs/heads/main" +
     window.location.pathname.split("cifras").pop().split("html")[0] + "m4a";
 
-    div.innerHTML = this.title + '<audio class="ms-4" controls="controls" ' +
-    'src="' + src + '" type="audio/mpeg"></audio>';
+    div.innerHTML = '<a class="link-light me-3" href="../index.html">'         +
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="cur' +
+    'rentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16"><path d="M6' +
+    '.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5' +
+    '.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5' +
+    '-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0' +
+    ' 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/></svg></a>'             +
+    this.title + '<audio class="ms-4" controls="controls" src="' + src +
+    '" type="audio/mpeg"></audio>';
 
     div.setAttribute("class", "d-flex align-items-center justify-content-" +
     "center text-center text-light bg-black shadow fs-5 p-4");
