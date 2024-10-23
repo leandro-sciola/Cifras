a496dfa59aa1d6fd3ff6ce94f742bc8455216da8
Leandro Sciola <leandrosciola@gmail.com>
2024-10-23 15:03:43 -0300

    Updated

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const div     = this.createElement("div");
     div.innerHTML = this.title +
     '<audio class="ms-4" controls="controls" src="' +
-    this.querySelector('meta[name="audio"]').content +
-    '" type="audio/mpeg"></audio>';
+    window.location.pathname.split("cifras").pop().split("html")[0] + 'm4a" ' +
+    'type="audio/mpeg"></audio>';
     div.setAttribute("class", "d-flex align-items-center justify-content-" +
         "center text-center text-light bg-black fs-5 p-4");
     this.body.prepend(div);
     pre.setAttribute("class", "fs-5 m-5");
-    pre.style.columnCount = this.querySelector('meta[name="columns"]').content;
+    pre.style.columnCount = pre.getAttribute("columns");
     pre.style.columnGap   = "30px";
     pre.style.whiteSpace  = "pre-wrap";
     Array.from(pre.querySelectorAll("b")).forEach(
