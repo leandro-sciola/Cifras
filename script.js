df87737b1d97705b35bb8a0479270686da82da02
Leandro Sciola <leandrosciola@gmail.com>
2024-10-23 15:20:50 -0300

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    const pre     = this.querySelector("pre");
-    const div     = this.createElement("div");
-    div.innerHTML = this.title +
-    '<audio class="ms-4" controls="controls" src="' +
-    window.location.pathname.split("cifras").pop().split("html")[0] + 'm4a" ' +
-    'type="audio/mpeg"></audio>';
+    const pre = this.querySelector("pre");
+    const div = this.createElement("div");
+    const src = "https://github.com/leandro-sciola/cifras/raw/refs/heads/main" +
+    window.location.pathname.split("cifras").pop().split("html")[0] + "m4a";
+
+    div.innerHTML = this.title + '<audio class="ms-4" controls="controls" ' +
+    'src="' + src + '" type="audio/mpeg"></audio>';
+
     div.setAttribute("class", "d-flex align-items-center justify-content-" +
-        "center text-center text-light bg-black fs-5 p-4");
+    "center text-center text-light bg-black shadow fs-5 p-4");
+
     this.body.prepend(div);
     pre.setAttribute("class", "fs-5 m-5");
     pre.style.columnCount = pre.getAttribute("columns");
